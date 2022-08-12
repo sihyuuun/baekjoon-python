8f658a1e70bd903695fba283f2b18140238ab26b
shyunshine <dnpsel426@naver.com>
2022-08-12 18:28:26 +0900

    2022.08.12

---
+++ 브루트 포스/2231.py
@@ -0,0 +1,14 @@
+﻿N = int(input())
+result = 0
+for i in range(1, N+1):
+    A = list(map(int, str(i)))
+    print(A)
+    result = i + sum(A)
+    print(result)
+
+    if result == N:
+        print(i)
+        break
+
+    if i == N:
+        print(0)
---
+++ 브루트 포스/2798.py
@@ -0,0 +1,12 @@
+﻿from itertools import combinations
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+
+big = 0
+
+for i in combinations(nums, 3):
+    temp = sum(i)
+    if big < temp <= m:
+        big = temp
+print(big)
